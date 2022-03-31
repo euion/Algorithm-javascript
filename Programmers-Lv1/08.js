3bb044c4c71c26698cc1bfe0a0056c4dd69170d8
0801euiyeon <qq212420@gmail.com>
2022-03-31 18:08:34 +0900

    11

--- Programmers-Lv1/08.js
+++ Programmers-Lv1/08.js
@@ -1,14 +1,13 @@
 //08: 음양 더하기
 function solution(absolutes, signs) {
-    var answer = 123456789;
-    var sum =0;
-    for(var i=0;i<absolutes.length; i++ ){
-        if(signs[i]===false){
-            absolutes[i] = absolutes[i]*(-1);
-
-        }
-        sum +=absolutes[i];
+  var answer = 123456789;
+  var sum = 0;
+  for (var i = 0; i < absolutes.length; i++) {
+    if (signs[i] === false) {
+      absolutes[i] = absolutes[i] * -1;
     }
-    answer = sum;
-    return answer;
+    sum += absolutes[i];
+  }
+  answer = sum;
+  return answer;
 }
